d8c236adf321948d9b72d6b1ac57c146052effbc
Fluorurine <votruong830430073@gmail.com>
2023-10-23 08:32:49 +0700

    Hoan thien co ban phan side drawer chi con cho Firebase

---
+++ lib/screens/home/side_drawer.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+class SideDrawer extends StatefulWidget {
+  UserProvider userProvider;
+  DrawerSide({this.userProvider});
+  const SideDrawer({super.key});
+
+  @override
+  _DrawerSideState createState() => _DrawerSideState();
+}
+
+class _DrawerSideState extends State<DrawerSide> {
+  Widget listTile({String title, IconData iconData, Function onTap}) {
+    return SizedBox(
+      height: 50,
+      child: ListTile(
+        onTap: onTap,
+        leading: Icon(
+          iconData,
+          size: 28,
+        ),
+        title: Text(
+          title,
+          style: TextStyle(color: textColor),
+        ),
+      ),
+    );
+  }
+
+// Is drawer Componet that divided list view into the first part is row with user profile and the second part is list of drawer. The user profile and data can be pull from
+  @override
+  Widget build(BuildContext context) {
+    var userData = widget.userProvider.currentUserData;
+    return Drawer(
+      child: Container(
+        color: primaryColor,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 43,
+                      backgroundColor: Colors.white54,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.yellow,
+                        backgroundImage: NetworkImage(
+                          userData.userImage ??
+                              "https://s3.envato.com/files/328957910/vegi_thumb.png",
+                        ),
+                        radius: 40,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(userData.userName),
+                        Text(
+                          userData.userEmail,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            listTile(
+              iconData: Icons.home_outlined,
+              title: "Home",
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(),
+                  ),
+                );
+              },
+            ),
+            listTile(
+              iconData: Icons.shop_outlined,
+              title: "Review Cart",
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => ReviewCart(),
+                  ),
+                );
+              },
+            ),
+            listTile(
+              iconData: Icons.person_outlined,
+              title: "My Profile",
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        MyProfile(userProvider: widget.userProvider),
+                  ),
+                );
+              },
+            ),
+            listTile(
+                iconData: Icons.notifications_outlined, title: "Notificatio"),
+            listTile(iconData: Icons.star_outline, title: "Rating & Review"),
+            listTile(
+                iconData: Icons.favorite_outline,
+                title: "Wishlist",
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => WishLsit(),
+                    ),
+                  );
+                }),
+            listTile(iconData: Icons.copy_outlined, title: "Raise a Complaint"),
+            listTile(iconData: Icons.format_quote_outlined, title: "FAQs"),
+            Container(
+              height: 350,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Contact Support"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text("Call us:"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("+923352580282"),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Text("Mail us:"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "assarbaloch5@gmail.com",
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
